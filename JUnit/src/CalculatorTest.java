5b7343d3e7cc25589862ed2fb32da0ef2b16b1a6
Challa-Venkata-Anirudh <anirudhchalla2004@gmail.com>
2025-06-29 23:09:17 +0530

    Week-2 [Initial Commit]

---
+++ JUnit/src/CalculatorTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    private Calculator calc;
+
+    @Before
+    public void setUp() {
+        calc = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        calc = null;
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calc.subtract(4, 3));
+    }
+}
---
+++ Mockito/src/MyServiceTest.java
@@ -0,0 +1,18 @@
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class MyServiceTest {
+
+    @Test
+    public void testExternalApi() {
+        ExternalApi mockApi = mock(ExternalApi.class);
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        MyService service = new MyService(mockApi);
+        String result = service.fetchData();
+
+        System.out.println("Result: " + result);  // <--- Direct Output
+        assertEquals("Mock Data", result);
+    }
+}
